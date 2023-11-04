36374dc5e04351a4491bec88c37b4ea1d9053643
Bagasadhe27 <bagas.adhe.21@student.is.ittelkom-sby.ac.id>
2023-11-04 21:45:25 +0700

    buttom tabs

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,54 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Cart, Home, Profile } from '../screens';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Text } from 'native-base';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color }) => {
+              
+              let iconName;
+              switch (route.name) {
+                case "Home":
+                  iconName = "home-outline";
+                  break;
+                case "Cart":
+                  iconName = "basket-outline";
+                  break;
+                case "Notifikasi":
+                  iconName = "notifications-outline";
+                  break;
+                case "Profile":
+                  iconName = "person-outline";
+                  break;
+              }
+              return (
+                <Ionicons
+                  name={iconName}
+                  size={28}
+                  color={focused ? "#3DB661" : color}
+                />
+              );
+            },
+            tabBarIconStyle: { marginTop: 5 },
+            tabBarStyle: {
+              height: 70,
+              borderTopWidth: 0,
+            },
+            tabBarLabel: ({ children, color, focused }) => {
+              return (
+                <Text color={focused ? "#3DB661" : color} mb={2}>
+                  {children}
+                </Text>
+              );
+            },
+          })}>
             <Tab.Screen
                 name="Home"
                 component={Home}
